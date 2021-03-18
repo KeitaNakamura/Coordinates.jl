b44aa37bbd5123bc38c7de0d321be2c2067f4630
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-18 12:42:52 +0900

    Allow to use any types of axes

--- src/Coordinates.jl
+++ src/Coordinates.jl
@@ -1,30 +1,13 @@
 module Coordinates
 
-using Base: @_propagate_inbounds_meta
+using Base: @_propagate_inbounds_meta, @_inline_meta
+using Base.Cartesian: @ntuple
 
-export AbstractCoordinate, Coordinate, coordinateaxes
-
-abstract type AbstractCoordinate{N, T} <: AbstractArray{NTuple{N, T}, N} end
-
-Base.size(A::AbstractCoordinate) = map(length, coordinateaxes(A))
-
-# getindex
-@inline function Base.getindex(A::AbstractCoordinate{N}, i::Vararg{Int, N}) where {N}
-    @boundscheck checkbounds(A, i...)
-    @inbounds broadcast(getindex, coordinateaxes(A), i)
-end
-@inline function Base.getindex(A::AbstractCoordinate{N}, I::Vararg{Union{AbstractUnitRange, Colon}, N}) where {N}
-    @boundscheck checkbounds(A, I...)
-    @inbounds typeof(A)(map(getindex, coordinateaxes(A), I))
-end
-@inline function Base.getindex(A::AbstractCoordinate{N}, I::CartesianIndices{N}) where {N}
-    @boundscheck checkbounds(A, I)
-    @inbounds A[I.indices...]
-end
+export Coordinate, coordinateaxes
 
 """
-    Coordinate(axes::AbstractVector...)
-    Coordinate{N}(axis::AbstractVector)
+    Coordinate(axes...)
+    Coordinate{N}(axis)
 
 Construct `Coordinate` from `axes`.
 If only single `axis` is given with specified number of dimensions `N`,
@@ -33,27 +16,66 @@ the `axis` is used for all dimensions.
 # Examples
 ```jldoctest
 julia> Coordinate(1:3, 2:4)
-3×3 Coordinate{2,Int64,UnitRange{Int64}}:
+3×3 Coordinate{2,Tuple{Int64,Int64},Tuple{UnitRange{Int64},UnitRange{Int64}}}:
  (1, 2)  (1, 3)  (1, 4)
  (2, 2)  (2, 3)  (2, 4)
  (3, 2)  (3, 3)  (3, 4)
 
 julia> Coordinate{2}(0.0:3.0)
-4×4 Coordinate{2,Float64,StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}}}:
+4×4 Coordinate{2,Tuple{Float64,Float64},Tuple{StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}},StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}}}}:
  (0.0, 0.0)  (0.0, 1.0)  (0.0, 2.0)  (0.0, 3.0)
  (1.0, 0.0)  (1.0, 1.0)  (1.0, 2.0)  (1.0, 3.0)
  (2.0, 0.0)  (2.0, 1.0)  (2.0, 2.0)  (2.0, 3.0)
  (3.0, 0.0)  (3.0, 1.0)  (3.0, 2.0)  (3.0, 3.0)
 ```
 """
-struct Coordinate{N, T, Axis <: AbstractVector{T}} <: AbstractCoordinate{N, T}
-    axes::NTuple{N, Axis}
+struct Coordinate{N, ElType, Axes <: Tuple{Vararg{Any, N}}} <: AbstractArray{ElType, N}
+    axes::Axes
+    function Coordinate{N, ElType, Axes}(axes::Axes) where {N, ElType, Axes}
+        check_coordinate_parameter(Val(N), ElType, Axes)
+        new{N, ElType, Axes}(axes)
+    end
 end
 
-Coordinate(axes::AbstractVector...) = Coordinate(axes)
-Coordinate{N}(ax::AbstractVector) where {N} = Coordinate(ntuple(i -> ax, Val(N)))
+@generated function check_coordinate_parameter(::Val{N}, ::Type{ElType}, ::Type{Axes}) where {N, ElType, Axes}
+    if Tuple{map(eltype, Axes.parameters)...} != ElType
+        return :(throw(ArgumentError("`ElType` and `eltype`s of given axes must match.")))
+    end
+end
+
+# constructors
+@generated function Coordinate(axes::Tuple{Vararg{Any, N}}) where {N}
+    ElType = Tuple{map(eltype, axes.parameters)...}
+    quote
+        Coordinate{N, $ElType, $axes}(axes)
+    end
+end
+Coordinate(axes...) = Coordinate(axes)
+Coordinate{N}(ax) where {N} = Coordinate(ntuple(i -> ax, Val(N)))
 
 coordinateaxes(C::Coordinate) = C.axes
 coordinateaxes(C::Coordinate, i::Int) = (@_propagate_inbounds_meta; coordinateaxes(C)[i])
+
+Base.size(A::Coordinate) = map(length, coordinateaxes(A))
+
+# getindex
+@generated function Base.getindex(A::Coordinate{N}, I::Vararg{Int, N}) where {N}
+    quote
+        @_inline_meta
+        @boundscheck checkbounds(A, I...)
+        @inbounds @ntuple $N i -> coordinateaxes(A, i)[I[i]]
+    end
+end
+@generated function Base.getindex(A::Coordinate{N}, I::Vararg{Union{AbstractUnitRange, Colon}, N}) where {N}
+    quote
+        @_inline_meta
+        @boundscheck checkbounds(A, I...)
+        @inbounds typeof(A)(@ntuple $N i -> coordinateaxes(A, i)[I[i]])
+    end
+end
+@inline function Base.getindex(A::Coordinate{N}, I::CartesianIndices{N}) where {N}
+    @boundscheck checkbounds(A, I)
+    @inbounds A[I.indices...]
+end
 
 end # module
