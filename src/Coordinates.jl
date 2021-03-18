e53d858fc7303b903ace9491f89b4195a6411e79
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-18 13:23:36 +0900

    Modify docs for Coordinate

--- src/Coordinates.jl
+++ src/Coordinates.jl
@@ -21,12 +21,26 @@ julia> Coordinate(1:3, 2:4)
  (2, 2)  (2, 3)  (2, 4)
  (3, 2)  (3, 3)  (3, 4)
 
-julia> Coordinate{2}(0.0:3.0)
-4×4 Coordinate{2,Tuple{Float64,Float64},Tuple{StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}},StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}}}}:
- (0.0, 0.0)  (0.0, 1.0)  (0.0, 2.0)  (0.0, 3.0)
- (1.0, 0.0)  (1.0, 1.0)  (1.0, 2.0)  (1.0, 3.0)
- (2.0, 0.0)  (2.0, 1.0)  (2.0, 2.0)  (2.0, 3.0)
- (3.0, 0.0)  (3.0, 1.0)  (3.0, 2.0)  (3.0, 3.0)
+julia> Coordinate(1:2, [3.0,4.0], "abc")
+2×2×3 Coordinate{3,Tuple{Int64,Float64,Char},Tuple{UnitRange{Int64},Array{Float64,1},String}}:
+[:, :, 1] =
+ (1, 3.0, 'a')  (1, 4.0, 'a')
+ (2, 3.0, 'a')  (2, 4.0, 'a')
+
+[:, :, 2] =
+ (1, 3.0, 'b')  (1, 4.0, 'b')
+ (2, 3.0, 'b')  (2, 4.0, 'b')
+
+[:, :, 3] =
+ (1, 3.0, 'c')  (1, 4.0, 'c')
+ (2, 3.0, 'c')  (2, 4.0, 'c')
+
+julia> Coordinate{2}(0:3)
+4×4 Coordinate{2,Tuple{Int64,Int64},Tuple{UnitRange{Int64},UnitRange{Int64}}}:
+ (0, 0)  (0, 1)  (0, 2)  (0, 3)
+ (1, 0)  (1, 1)  (1, 2)  (1, 3)
+ (2, 0)  (2, 1)  (2, 2)  (2, 3)
+ (3, 0)  (3, 1)  (3, 2)  (3, 3)
 ```
 """
 struct Coordinate{N, ElType, Axes <: Tuple{Vararg{Any, N}}} <: AbstractArray{ElType, N}
