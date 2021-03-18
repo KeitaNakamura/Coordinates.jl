922dae7289bd20ad0d9f09b805c337ca927afca0
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-18 12:50:21 +0900

    Allow to use any types of axes (#2)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,37 @@
 using Coordinates
 using Test
 
-function check_coordinate(coord::Coordinate{N, T}, axes::Tuple) where {N, T}
+function check_coordinate(coord::Coordinate{N, ElType}, axes::Tuple) where {N, ElType}
     @test all(CartesianIndices(coord)) do I
-        (@inferred coord[I])::NTuple{N, T} == getindex.(axes, Tuple(I))
+        (@inferred coord[I])::ElType == getindex.(axes, Tuple(I))
     end
     @test size(coord) == length.(axes)
     @test coordinateaxes(coord) == axes
 end
 check_coordinate(coord::Coordinate{N}, ax) where {N} = check_coordinate(coord, ntuple(i -> ax, Val(N)))
 
-@testset "Constructors" begin
-    for T in (Float32, Float64), N in (1,2,3)
-        # from tuple
-        for axs in (ntuple(i -> 1:rand(T(1):10), N),
-                    ntuple(i -> 1:rand(T):rand(2:10), N),
-                    ntuple(i -> rand(T, rand(1:10)), N),)
-            C = (@inferred Coordinate(axs))::Coordinate{N, T, eltype(axs)}
-            C = (@inferred Coordinate(axs...))::Coordinate{N, T, eltype(axs)}
-            check_coordinate(C, axs)
-        end
-        # from axis
-        for axis in (1:rand(T(1):10),
-                     1:rand(T):rand(2:10),
-                     rand(T, rand(1:10)),)
-            C = (@inferred Coordinate{N}(axis))::Coordinate{N, T, typeof(axis)}
+@testset "Coordinate" begin
+    @test (@inferred Coordinate{2, Tuple{Int, Float64}, Tuple{UnitRange{Int}, Vector{Float64}}}((1:2, [2.0,3.0]))).axes == (1:2, [2.0,3.0])
+    @test (@inferred Coordinate{2, Tuple{Int, Char}, Tuple{UnitRange{Int}, String}}((1:2, "abc"))).axes == (1:2, "abc")
+    @test_throws ArgumentError Coordinate{2, Tuple{Float64, Float64}, Tuple{UnitRange{Int}, Vector{Float64}}}((1:2, [2.0,3.0]))
+    # from axes
+    for axes in ((1:3,),
+                (1:3, 2:4),
+                (1:3, 2:4, 5:8),
+                (1:3, 2:2.0:4, "abcd"),
+                (1:3, [1,2,3.0], "abcd"))
+        N = length(axes)
+        ElType = Tuple{map(eltype, axes)...}
+        C = (@inferred Coordinate(axes))::Coordinate{N, ElType, typeof(axes)}
+        C = (@inferred Coordinate(axes...))::Coordinate{N, ElType, typeof(axes)}
+        check_coordinate(C, axes)
+    end
+    # from axis
+    for axis in (1:3, [2,4,4.0], "abcd")
+        for N in 1:3
+            axes = ntuple(i -> axis, Val(N))
+            ElType = Tuple{map(eltype, axes)...}
+            C = (@inferred Coordinate{N}(axis))::Coordinate{N, ElType, typeof(axes)}
             check_coordinate(C, axis)
         end
     end
