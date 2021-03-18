81a07f1edf436e0323ca90cff06b7a3d6e8707da
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-18 13:52:54 +0900

    Modify docs

--- src/Coordinates.jl
+++ src/Coordinates.jl
@@ -10,7 +10,7 @@ export Coordinate, coordinateaxes
     Coordinate{N}(axis)
 
 Construct `Coordinate` from `axes`.
-If only single `axis` is given with specified number of dimensions `N`,
+If only single `axis` is given with number of dimensions `N`,
 the `axis` is used for all dimensions.
 
 # Examples
